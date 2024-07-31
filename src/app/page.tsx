98440349a74d7930379076d17c397bf02e9291be
SiddharthManjul <manjulsiddharth09@gmail.com>
2024-07-31 11:12:46 +0530

    Tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,77 @@ import ShimmerButton from "@/components/magicui/shimmer-button";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="bg-cyan-700">
+      <div className="bg-cyan-300 w-full flex flex-wrap items-center justify-between px-16 py-8 rounded-lg space-y-2">
         <div className="z-10 flex items-center justify-center">
           <ShimmerButton className="shadow-2xl">
             <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
-              Shimmer Button
+              Image/png
             </span>
           </ShimmerButton>
         </div>
-        ̦̦
+        <div className="z-10 flex items-center justify-center">
+          <ShimmerButton className="shadow-2xl">
+            <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+              Image/png
+            </span>
+          </ShimmerButton>
+        </div>
+        <div className="z-10 flex items-center justify-center">
+          <ShimmerButton className="shadow-2xl">
+            <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+              Image/png
+            </span>
+          </ShimmerButton>
+        </div>
+        <div className="z-10 flex items-center justify-center">
+          <ShimmerButton className="shadow-2xl">
+            <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+              Image/png
+            </span>
+          </ShimmerButton>
+        </div>
+        <div className="z-10 flex items-center justify-center">
+          <ShimmerButton className="shadow-2xl">
+            <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+              Image/png
+            </span>
+          </ShimmerButton>
+        </div>
+        <div className="z-10 flex items-center justify-center">
+          <ShimmerButton className="shadow-2xl">
+            <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+              Image/png
+            </span>
+          </ShimmerButton>
+        </div>
+        <div className="z-10 flex items-center justify-center">
+          <ShimmerButton className="shadow-2xl">
+            <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+              Image/png
+            </span>
+          </ShimmerButton>
+        </div>
+        <div className="z-10 flex items-center justify-center">
+          <ShimmerButton className="shadow-2xl">
+            <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+              Image/png
+            </span>
+          </ShimmerButton>
+        </div>
+        <div className="z-10 flex items-center justify-center">
+          <ShimmerButton className="shadow-2xl">
+            <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+              Image/png
+            </span>
+          </ShimmerButton>
+        </div>
+        <div className="z-10 flex items-center justify-center">
+          <ShimmerButton className="shadow-2xl">
+            <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+              Image/png
+            </span>
+          </ShimmerButton>
+        </div>
       </div>
     </main>
   );
